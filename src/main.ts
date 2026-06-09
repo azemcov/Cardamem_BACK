a69aecdfb97977f513a7dccc10b3ca5c919f1080
Alexandr Zemtsov <a.zemcov@gmail.com>
2026-06-09 16:41:56 +0300

    fix: решён баг с кнопками из-за next 16

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const сonfigService = app.get(ConfigService);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: сonfigService.get<string>('FRONTEND_URL'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
